ef829d0cdfb4d47b2735c9c0ffe0828485391cd9
Alinxus <alexstunner2007@gmail.com>
2025-07-14 18:38:54 +0100

    latest

--- src/app/(site)/Navbar.tsx
+++ src/app/(site)/Navbar.tsx
@@ -30,12 +30,7 @@ export default function Navbar() {
       <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
         <div className="flex items-center">
           <Link href="/" className="flex items-center gap-2">
-            <Zap
-              className="h-10 w-10"
-              fill="#FFBE18"
-              stroke="black"
-              strokeWidth={1.4}
-            />
+            <img src="/logo.png" alt="Atmet" />
             <span className="text-lg font-semibold text-white">Atmet</span>
           </Link>
         </div>
